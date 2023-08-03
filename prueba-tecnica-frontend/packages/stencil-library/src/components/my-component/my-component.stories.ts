edace82545f7b65d4a3b2858faa88401455c4f87
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2023-08-02 20:01:11 -0500

    build: configure storybook

---
+++ prueba-tecnica-frontend/packages/stencil-library/src/components/my-component/my-component.stories.ts
@@ -0,0 +1,21 @@
+interface MyComponentArgs {
+  first: string;
+  middle: string;
+  last: string;
+}
+
+export default {
+  // this creates a ‘Components’ folder and a ‘MyComponent’ subfolder
+  title: 'Components/MyComponent',
+  tags: ['autodocs'],
+};
+
+const Template = (args: MyComponentArgs) => `<my-component first="${args.first}" middle="${args.middle}" last="${args.last}"></my-component>`;
+
+export const Example = Template.bind({});
+
+Example.args = {
+  first: 'Winnie',
+  middle: 'The',
+  last: 'Pooh'
+};
