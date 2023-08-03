2dc1fc84780e691b45300dc087761a65baa40da3
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2023-08-03 08:50:53 -0500

    feat: add message in RtCardsFetched

--- prueba-tecnica-frontend/packages/stencil-library/src/components/rt-cards-fetched/rt-cards-fetched.stories.tsx
+++ prueba-tecnica-frontend/packages/stencil-library/src/components/rt-cards-fetched/rt-cards-fetched.stories.tsx
@@ -3,6 +3,9 @@ export default {
   tags: ['autodocs'],
 };
 
-const Template = () => `<rt-cards-fetched></rt-cards-fetched>`;
+const Template = () => `
+  <rt-cards-fetched></rt-cards-fetched>
+  <p style="margin-top: 10px;">Nota: En caso no se muestre nada, es porque no hay algún usuario creado.</p>
+`;
 
 export const Example = Template.bind({});
