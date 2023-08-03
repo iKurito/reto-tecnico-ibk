0b19dd97d1f2ed692421e8c0f0d0eaf29d2294f6
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2023-08-02 20:38:47 -0500

    feat: add user page

--- prueba-tecnica-frontend/packages/angular-workspace/src/app/users/pages/user-page/user-page.component.ts
+++ prueba-tecnica-frontend/packages/angular-workspace/src/app/users/pages/user-page/user-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-user-page',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-page.component.sass']
 })
 export class UserPageComponent implements OnInit {
+  public user?: User;
+  public uid: string = '';
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => {
+        this.uid = id;
+        return this.usersService.getUserById(id);
+      }))
+      .subscribe(user => {
+        if (!user) return this.router.navigate(['/users/list']);
+        this.user = user;
+        this.uid = user.uid
+        return;
+      });
   }
 
 }
